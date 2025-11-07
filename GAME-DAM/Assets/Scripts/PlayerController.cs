cd93b89fccb29c5160cc07e8babce92776ad4b27
Iván Hernández <navihf05@gmail.com>
2025-11-07 12:05:55 +0100

    Mejora del Script de movimiento, implementado el doble salto

---
+++ GAME-DAM/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+
+
+
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+
+        if (facingRight == false && moveInput > 0){
+            flip();
+        }else if (facingRight == true && moveInput < 0){
+            flip();
+        }
+    }
+
+    void Update()
+    {
+       if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.linearVelocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.linearVelocity = Vector2.up * jumpForce;
+
+        }
+    }
+   
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+}
+
